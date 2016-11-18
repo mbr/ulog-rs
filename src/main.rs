9cf60712efaca681aa284a247a8bf5e6265183a7
Marc Brinkmann <git@marcbrinkmann.de>
2016-11-18 16:13:22 +0100

    Added Async capabilities.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
+extern crate log;
+extern crate thread_local;
+
+use log::{LogRecord, LogLevel, LogMetadata};
+
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Mutex;
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 
-pub struct LogMsg {
 
+use thread_local::ThreadLocal;
+
+// thread_local!(static FOO: usize = 123);
+
+pub struct LogMessage {
+
+}
+
+impl LogMessage {
+    fn new() -> LogMessage {
+        LogMessage {}
+    }
 }
 
 pub struct AsyncLogger {
     failures: AtomicUsize,
-    sender: SyncSender<LogMsg>,
+
+    // sender is only used once to clone for each thread
+    sender: Mutex<SyncSender<LogMessage>>,
+
+    // use thread-local storage for local instance
+    local_sender: thread_local::ThreadLocal<SyncSender<LogMessage>>,
 }
 
 impl AsyncLogger {
-    pub fn new(bufsize: usize) -> (AsyncLogger, Receiver<LogMsg>) {
+    pub fn new(bufsize: usize) -> (AsyncLogger, Receiver<LogMessage>) {
         let (tx, rx) = sync_channel(bufsize);
 
         (AsyncLogger {
             failures: AtomicUsize::new(0),
-            sender: tx,
+            sender: Mutex::new(tx),
+            local_sender: ThreadLocal::new(),
         },
          rx)
     }
@@ -29,6 +52,33 @@ impl AsyncLogger {
     #[inline]
     pub fn failures(&self) -> usize {
         self.failures.load(Ordering::Relaxed)
+    }
+
+    #[inline]
+    pub fn sender(&self) -> &SyncSender<LogMessage> {
+        // check if we already have a suitable sender, if not, we need to clone
+        // it
+
+        self.local_sender.get_or(|| {
+            let guard = self.sender.lock().unwrap();
+            let local_tx = guard.clone();
+            Box::new(local_tx)
+        })
+    }
+}
+
+impl log::Log for AsyncLogger {
+    fn enabled(&self, metadata: &LogMetadata) -> bool {
+        // FIXME
+        metadata.level() <= LogLevel::Debug
+    }
+
+    fn log(&self, record: &LogRecord) {
+        let msg = LogMessage::new();
+        if let Err(_) = self.sender().try_send(msg) {
+            // failed to send log message; increase failure count
+            self.failures.fetch_add(1, Ordering::Relaxed);
+        }
     }
 }
 
