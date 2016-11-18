2ed2c731b84c0bd2f35cad1d86bd07a0f6741f47
Marc Brinkmann <git@marcbrinkmann.de>
2016-11-18 18:46:21 +0100

    Added docs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+//! ulog: Flexible asynchronous logging
+//!
+//! ## Features:
+//!
+//! * Asynchronous: `ulog` sends all log messages through a channel to another
+//!   thread as soon as possible, trying to minimize the time the logging
+//!   thread spends processing the log.
+//! * Standard `log`: `ulog` uses the de-facto standard logging facade of
+//!   [log](https://crates.io/crates/log), allowing access to familiar
+//!   features.
+//! * Flexible: `ulog` is small but flexible, allowing different use cases
+//!   other than standard file or stream logging. Log handling is done by
+//!   passing a closure that is executed in a different thread.
+//!
+//! ## Intended use and example
+//!
+//! `ulog` is intended for use on embedded Linux applications that are
+//! multithreaded and require a single thread to provide hard real-time
+//! guarantees. Logs made using `ulog` are sent through a [synchronous
+//! channel](https://doc.rust-lang.org/std/sync/mpsc/fn.sync_channel.html)
+//! before being processed, if logging cannot be completed in constant time,
+//! the message is dropped and an error flag is set.
+//!
+//! ### Example use case
+//!
+//! Consider an application controlling a piece of hardware that has moderate
+//! real-time guarantees (in the 10s of milliseconds). Allocations and other
+//! execution will usually run just fine, however writing logs to disk or
+//! stderr can occasionally slow down the process by hundres of millliseconds
+//! under some conditions.
+//!
+//! To avoid this, `ulog` can be used to offloaded heavy I/O into a separate
+//! thread, if enough execution threads are available and scheduling is setup
+//! correctly, this will guarantee that logging never takes longer than a
+//! string format and a few small allocations.
+
 #[macro_use]
 extern crate log;
 extern crate thread_local;
@@ -10,9 +46,19 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Mutex;
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 
-
 use thread_local::ThreadLocal;
 
+/// A log entry
+///
+/// The `LogMessage` struct copies most interesting fields from a
+/// `log::LogRecord`, allowing owned versions of these to be passed around.
+///
+/// Any time a `LogMessage` is constructed, the following allocations need to
+/// be made:
+///
+/// * Four pointer-sized fields (`level`, as well as three inside `location`)
+/// * An owned copy of `target`
+/// * The result of formatting the message (see `LogRecord.args()`).
 #[derive(Debug)]
 pub struct LogMessage {
     pub level: LogLevel,
@@ -27,6 +73,16 @@ impl fmt::Display for LogMessage {
     }
 }
 
+/// Asynchronous logger
+///
+/// The very first time a message is logged on a channel in a thread, a copy of
+/// the sending end of the channel is made, requiring a single mutex lock and
+/// clone. For further calls, this sending end is cached as a thread local.
+///
+/// ## Panics
+///
+/// Logging may panic in the unlikely event the lock becomes poisoned upon
+/// logging from a new thread.
 pub struct AsyncLogger {
     failures: AtomicUsize,
 
@@ -38,6 +94,10 @@ pub struct AsyncLogger {
 }
 
 impl AsyncLogger {
+    /// Creates a new logger.
+    ///
+    /// `bufsize` denotes the queue size before messages are
+    /// dropped without being handled.
     pub fn new(bufsize: usize) -> (AsyncLogger, Receiver<LogMessage>) {
         let (tx, rx) = sync_channel(bufsize);
 
@@ -49,18 +109,14 @@ impl AsyncLogger {
          rx)
     }
 
-    #[inline]
-    pub fn failed(&self) -> bool {
-        self.failures() != 0
-    }
-
+    /// Number of dropped log messages.
     #[inline]
     pub fn failures(&self) -> usize {
         self.failures.load(Ordering::Relaxed)
     }
 
     #[inline]
-    pub fn sender(&self) -> &SyncSender<LogMessage> {
+    fn sender(&self) -> &SyncSender<LogMessage> {
         // check if we already have a suitable sender, if not, we need to clone
         // it
 
@@ -94,7 +150,12 @@ impl log::Log for AsyncLogger {
     }
 }
 
-fn init<F>(bufsize: usize, handle: F) -> Result<(), log::SetLoggerError>
+/// Initialize ulog logging.
+///
+/// Creates a new `AsyncLogger` with a buffered channel of size `bufsize` and
+/// starts a handler thread that will call `handle` for every `LogMessage`
+/// received.
+pub fn init<F>(bufsize: usize, handle: F) -> Result<(), log::SetLoggerError>
     where F: Fn(LogMessage) -> (),
           F: Send + 'static
 {
@@ -118,6 +179,12 @@ fn init<F>(bufsize: usize, handle: F) -> Result<(), log::SetLoggerError>
     })
 }
 
+/// Initialize ulog loggin with default `stderr`-based logging.
+///
+/// Sets a buffer size of 128 log entries.
+///
+/// ## Panics
+/// Will panic if stderr is unavailable for writing.
 fn init_stderr() -> Result<(), log::SetLoggerError> {
     init(128, move |msg| {
         writeln!(&mut std::io::stderr(), "{}", msg).expect("Error writing
